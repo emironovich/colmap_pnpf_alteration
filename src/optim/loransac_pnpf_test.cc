6b7d4b9154f1070fe1008ee0a0ca84a6b5d7120a
Elizaveta Mironovich <elizavetamironvich@yandex.ru>
2020-03-04 12:15:08 +0300

    Fixed P3.5Pf LORANSAC test

--- src/optim/loransac_pnpf_test.cc
+++ src/optim/loransac_pnpf_test.cc
@@ -29,18 +29,18 @@
 //
 // Author: Johannes L. Schoenberger (jsch-at-demuc-dot-de)
 
-#define TEST_NAME "optim/ransac" // todo what is that?????
+#define TEST_NAME "optim/loransac_pnpf"
 #include "util/testing.h"
 
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
 #include "base/pose.h"
+#include "estimators/absolute_pose.h"
+#include "estimators/absolute_pose_pnpf.h"
 #include "optim/loransac.h"
 #include "optim/loransac_pnpf.h"
 #include "util/random.h"
-#include "estimators/absolute_pose_pnpf.h"
-#include "estimators/absolute_pose.h"
 #include <random>
 #include <unsupported/Eigen/MatrixFunctions>
 
@@ -54,7 +54,7 @@ BOOST_AUTO_TEST_CASE(TestP35PLORANSAC) {
 
   // Create some arbitrary transformation.
   std::random_device dev;
-  std::mt19937_64 generator(dev()); // Mersenne Twister generator
+  std::mt19937_64 generator(dev());  // Mersenne Twister generator
   std::uniform_real_distribution<double> uniformDistribution(-1., 1.);
   auto uniform = [&]() { return uniformDistribution(generator); };
 
@@ -62,8 +62,8 @@ BOOST_AUTO_TEST_CASE(TestP35PLORANSAC) {
   double f = 200 + 1800 * (uniformDistribution(generator) + 1) / 2;
 
   // rotation
-  Eigen::Vector4d rQuat =  Eigen::Vector4d::NullaryExpr(4, 1, uniform);
-  Eigen::Matrix3d R =  QuaternionToRotationMatrix(rQuat);
+  Eigen::Vector4d rQuat = Eigen::Vector4d::NullaryExpr(4, 1, uniform);
+  Eigen::Matrix3d R = QuaternionToRotationMatrix(rQuat);
 
   // camera center
   Eigen::Vector3d C = (Eigen::Vector3d::NullaryExpr(3, 1, uniform)) / 2;
@@ -86,20 +86,20 @@ BOOST_AUTO_TEST_CASE(TestP35PLORANSAC) {
   for (size_t i = 0; i < num_samples; ++i) {
     Eigen::Vector3d pnt;
     pnt << 0, 0, 6;
-    pnt += 2*(Eigen::Vector3d::NullaryExpr(3, 1, uniform));
-    points3D.emplace_back(R.transpose()*pnt + C);
-    points2D.emplace_back((P*(points3D.back()).homogeneous()).hnormalized());
+    pnt += 2 * (Eigen::Vector3d::NullaryExpr(3, 1, uniform));
+    points3D.emplace_back(R.transpose() * pnt + C);
+    points2D.emplace_back((P * (points3D.back()).homogeneous()).hnormalized());
   }
 
   // Add some faulty data.
   for (size_t i = 0; i < num_outliers; ++i) {
     points2D[i] = Eigen::Vector2d(RandomReal(-3000.0, -2000.0),
-                                   RandomReal(-4000.0, -3000.0));
+                                  RandomReal(-4000.0, -3000.0));
   }
 
   // Robustly estimate transformation using RANSAC.
   RANSACOptions options;
-  options.max_error = 10;
+  options.max_error = 1;
   LORANSAC<P35PfEstimator, EPNPEstimator> ransac(options);
   const auto report = ransac.Estimate(points2D, points3D);
 
@@ -118,15 +118,15 @@ BOOST_AUTO_TEST_CASE(TestP35PLORANSAC) {
 
   // Make sure original parameters were estimated correctly.
 
-  //Test if correct focal length has been found.
+  // Test if correct focal length has been found.
   const double f_diff = abs(f - (report.model).f) / f;
   BOOST_CHECK(f_diff < 1e-6);
 
   // Test if correct rotation has been determined.
-  const double R_diff = (R - (report.model).R).norm()/ 3;
+  const double R_diff = (R - (report.model).R).norm() / 3;
   BOOST_CHECK(R_diff < 1e-4);
 
   // Test if correct translation has been determined.
-  const double T_diff = (-R*C - (report.model).T).norm();
+  const double T_diff = (-R * C - (report.model).T).norm();
   BOOST_CHECK(T_diff < 1e-3);
 }
